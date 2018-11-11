783332d8873ff2087926824ef8b7175beebf8196
jimisun <503606090@qq.com>
2018-11-11 17:13:53 +0800

    add 第一张:Struts2-Struts2起步示例源码

---
+++ 第一章：Struts2/1.Struts2起步/src/main/java/com/jimisun/action/TestAction.java
@@ -0,0 +1,14 @@
+package com.jimisun.action;
+
+/**
+ * @Author:jimisun
+ * @Description:
+ * @Date:Created in 16:04 2018-11-11
+ * @Modified By:
+ */
+public class TestAction {
+
+    public String execute() throws Exception {
+        return "hello";
+    }
+}
